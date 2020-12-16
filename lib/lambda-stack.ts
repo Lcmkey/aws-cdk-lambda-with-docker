7d96b686bf72dade3fa21420bb4c86db01da154d
Lcmkey <lcmkey@gmail.com>
2020-12-16 22:53:11 +0800

    mutiple lambda deployment stack

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,6 +1,8 @@
-import { App, Stack, StackProps } from "@aws-cdk/core";
-import { DockerImageFunction, DockerImageCode } from "@aws-cdk/aws-lambda";
+import { App, Stack, StackProps, CfnOutput } from "@aws-cdk/core";
+import { DockerImageFunction, DockerImageCode, Function } from "@aws-cdk/aws-lambda";
 import * as path from "path";
+import * as fs from 'fs'
+import * as camelcase from 'camelcase';
 
 interface LambdaStackStackProps extends StackProps {
   readonly prefix: string;
@@ -8,6 +10,8 @@ interface LambdaStackStackProps extends StackProps {
 }
 
 export class LambdaStack extends Stack {
+  public functionList: { [key: string]: Function } = {}
+
   constructor(scope: App, id: string, props: LambdaStackStackProps) {
     super(scope, id, props);
 
@@ -19,15 +23,39 @@ export class LambdaStack extends Stack {
     /**
      * Configure path to Dockerfile
      */
-    const dockerfile = path.join(__dirname, "./../src");
+    // const dockerfile = path.join(__dirname, "./../src/lambda");
 
     /**
+     * Signle Lambda
      * Create AWS Lambda function and push image to ECR
      */
-    new DockerImageFunction(this, `${prefix}-${stage}-Handler`, {
-      functionName: `${prefix}-${stage}-Handler`,
-      memorySize: 256,
-      code: DockerImageCode.fromImageAsset(dockerfile),
+    // new DockerImageFunction(this, `${prefix}-${stage}-Handler`, {
+    //   functionName: `${prefix}-${stage}-Handler`,
+    //   memorySize: 256,
+    //   code: DockerImageCode.fromImageAsset(dockerfile),
+    // });
+
+    /**
+     * Define Lmabda handler sroure code path
+     */
+    const handlersPath = path.join(__dirname, "./../src/lambda");
+    const nodes = fs.readdirSync(handlersPath);
+
+    /**
+     * Create Mutiple Lambda
+     */
+    nodes.filter((node) => fs.statSync(`${handlersPath}/${node}`).isDirectory()).map((name) => {
+      const id = camelcase(name, { pascalCase: true });
+
+      this.functionList[id] = new DockerImageFunction(this, `${prefix}-${stage}-Handler-${id}`, {
+        functionName: `${prefix}-${stage}-Handler-${id}`,
+        memorySize: 256,
+        code: DockerImageCode.fromImageAsset(`${handlersPath}/${name}`),
+      });
+
+      new CfnOutput(this, `$${prefix}-${stage}-Handler-${id}-Arn`, {
+        value: this.functionList[id].functionArn
+      })
     });
   }
 }
